240eb1fe42e700657f001f8675d89c72a6ca9bc7
Maximo Gomer <maximogomer02@gmail.com>
2025-06-09 16:04:03 -0300

    primer instancia gui

--- script.js
+++ script.js
@@ -17,76 +17,20 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let choice = prompt("Please enter Rock, Paper or Scissors");
-    
+let buttons = document.querySelector("#buttonsPlayer");
 
-    if (choice === null){
-        return "Please enter a value";
+buttons.addEventListener("click",(event) =>{
+    let target = event.target;
+
+    switch(target.id) {
+        case "buttonRock":
+            console.log("Rock");
+            break;
+        case "buttonPaper":
+            console.log("Paper");
+            break;
+        case "buttonScissors":
+            console.log("Scissors");
+            break;
     }
-    else{
-        choice.toLowerCase();
-        if(choice == "rock" || choice == "paper" || choice == "scissors"){
-            return choice;
-        }
-        else {
-            return "Please enter a valid value";
-        }
-    }
-    
-}
-
-function playRound(humanChoice,computerChoice) {
-    if(humanChoice == "rock"){
-        if(computerChoice == "rock"){
-            return "Draw";
-        }
-        else if(computerChoice == "scissors"){
-            humanScore++;
-            return "Victory";
-        }
-        else{
-            computerScore++;
-            return "Defeat";
-        }
-    }
-    else if(humanChoice == "paper"){
-        if(computerChoice == "paper"){
-            return "Draw";
-        }
-        else if(computerChoice == "rock"){
-            humanScore++;
-            return "Victory";
-        }
-        else{
-            computerScore++;
-            return "Defeat";
-        }
-    }
-    else{
-        if(computerChoice == "scissors"){
-            return "Draw";
-        }
-        else if(computerChoice == "paper"){
-            humanScore++;
-            return "Victory";
-        }
-        else{
-            computerScore++;
-            return "Defeat";
-        }
-    }
-}
-
-let humanScore = 0;
-let computerScore = 0;
-
-const computerChoice = getComputerChoice();
-const humanChoice = getHumanChoice();
-
-console.log(humanChoice);
-console.log(computerChoice);
-
-console.log(playRound(humanChoice,computerChoice));
-console.log(humanScore);
-console.log(computerScore);
+});
